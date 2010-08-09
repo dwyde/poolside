d53ba3a2675a5258d2b93f9aa8ec5842c46c9094
David Wyde <david.wyde@gmail.com>
2010-08-09 17:23:29 -0600

    Cleaned up interaction.js.

--- docs/purejs/_attachments/interaction.js
+++ docs/purejs/_attachments/interaction.js
@@ -1,31 +1,4 @@
 $(document).ready(function(){
-    $("button#new").click(function(){
-        append_cell();
-    });
-    
-    $("button#delete").click(function(){
-        var worksheet = $("input[@id=worksheet]:checked").val();
-        if (worksheet != undefined) {
-            var accepted = confirm("Are you sure you want to delete worksheet \"" + worksheet + "\"?");
-            if (accepted) {
-                $("form#worksheet_action input#action").val('delete');
-                $("form#worksheet_action").submit();
-            }
-        }
-    });
-    
-    $("button#copy").click(function(){
-        var worksheet = $("input[@id=worksheet]:checked").val();
-        if (worksheet != undefined) {
-            var name = prompt("Please enter a name for the new worksheet.");
-            if (name != undefined) {
-                $("form#worksheet_action input#action").val('copy');
-                $("form#worksheet_action input#new_name").val(name);
-                $("form#worksheet_action").submit();
-            }
-        }
-    });
-    
     function append_cell() {
        var worksheet = $("div#worksheet");
        var cell = '<form class="cell" method="POST"> \
@@ -36,6 +9,10 @@ $(document).ready(function(){
        //cell_count += 1;
        worksheet.append(cell);
     }
+    
+    $("button#new").click(function(){
+        append_cell();
+    });
     
     $("form.cell").live('submit', function(){
         var form = $(this);
