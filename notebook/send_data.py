fbda897383689fad963795a8bc78eacfc3c6919f
David Wyde <dwyde@hartlab.ncf.edu>
2010-09-06 17:59:40 -0400

    Deleted a script that couchapp doesn't need.

--- notebook/send_data.py
+++
@@ -1,8 +0,0 @@
-import couchdb
-
-
-couch = couchdb.Server('http://localhost:59840/')
-db = couch['nb']
-db.create({'_id': 'cell_a', 'type': 'cell', 'input': '"hello"', 'output': 'hello'})
-db.create({'_id': 'cell_b', 'type': 'cell', 'input': '1 + 1', 'output': '2'})
-db.create({'_id': 'worksheet_1', 'type': 'worksheet', 'cells': ['cell_b', 'cell_a']})
