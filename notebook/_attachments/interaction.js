d88367f891e8d86f0c4bc89773bf76af5d4eeeeb
David <david@computer.(none)>
2010-10-26 23:13:32 -0400

    Accidentally undid updates to "interaction.js", and now I'm putting them back.

--- notebook/_attachments/interaction.js
+++ notebook/_attachments/interaction.js
@@ -5,30 +5,24 @@ $(document).ready(function(){
     var EVAL_SERVER = [DB_NAME, '_service'].join(PATH_SEP);
 
     var WORKSHEET_NAME = window.location.pathname.split('/').pop();
+    var JSON_VERSION = '1.1';
     
-    function ajax_json(params) {
-        $.ajax({
-            'url': params['url'],
-            'data': params['data'],
-            'success': params['success'],
-            'global': false,
-            'contentType': 'application/json',
-            'type': 'POST',
-            'processData': false,
-            'dataType': 'json',
-        });
-    }
+    $.ajaxSetup({
+        'global': false,
+        'contentType': 'application/json',
+        'type': 'POST',
+        'processData': false,
+        'dataType': 'json',
+    });
     
     function save_cell(id, input, output) {
-        var json_data = {
-            'version': '1.1', 
-            'method': 'save_cell', 
-            'params': {'cell_id': id, 'input': input, 'output': output},
-        };
-
-        ajax_json({
+        $.ajax({
             url: EVAL_SERVER, 
-            data: JSON.stringify(json_data), 
+            data: JSON.stringify({
+                method: 'save_cell', 
+                params: {cell_id: id, input: input, output: output},
+                version: JSON_VERSION, 
+            }), 
             success: function(msg){
                 if ($('#' + id).length == 0) {
                     alert('No cell with id ' + id);
@@ -38,37 +32,30 @@ $(document).ready(function(){
     }
     
     function save_worksheet() {
-        cell_list = $('div#worksheet')
+        cells = $('#worksheet')
             .children('.cell')
             .map(function() {
                 return this.id;
             }).get();
         
-        var json_data = {
-            'version': '1.1', 
-            'method': 'save_worksheet',
-            'params': {'cell_list': cell_list, 'worksheet_id': WORKSHEET_NAME},
-        };
-        
-        ajax_json({
+        $.ajax({
             url: EVAL_SERVER, 
-            data: JSON.stringify(json_data), 
-            success: function(msg){
-            },
+            data: JSON.stringify({
+                'method': 'save_worksheet',
+                'params': {'cell_list': cells, 'worksheet_id': WORKSHEET_NAME},
+                'version': JSON_VERSION, 
+            }),
         });
     }
     
-    $('button#add_cell').click(function(){
-	// In an external file, to be shared with a list function.
-        var json_data = {
-            'version': '1.1',
-            'method': 'new_id',
-            'params': {},
-        };
-
-        ajax_json({
+    $('#add_cell').click(function(){
+        $.ajax({
             url: EVAL_SERVER,
-            data: JSON.stringify(json_data),
+            data: JSON.stringify({
+                'method': 'new_id',
+                'params': {},
+                'version': JSON_VERSION,
+            }),
             success: function(msg){
                 var id = msg.result;
                 var cell = new_cell(id, '', '');
@@ -79,17 +66,15 @@ $(document).ready(function(){
         });
     });
 
-    $('.cell .delete').live('click', function(){
+    $('button.delete').live('click', function(){
         var cell = $(this).parent();
-        var json_data = {
-            'version': '1.1',
-            'method': 'delete_cell',
-            'params': {'cell_id': cell.attr('id')},
-        }
-        
-        ajax_json({
+        $.ajax({
             'url': EVAL_SERVER,
-            'data': JSON.stringify(json_data),
+            'data': JSON.stringify({
+                'method': 'delete_cell',
+                'params': {'cell_id': cell.attr('id')},
+                'version': JSON_VERSION,
+            }),
             'success': function(msg){
                 cell.remove();
                 save_worksheet();
@@ -101,16 +86,13 @@ $(document).ready(function(){
     $('form.cell').live('submit', function(){
         var form = $(this);
         var input = form.children('.input').val();
-        
-        var json_data = {
-            'version': '1.1', 
-            'method': 'eval_python', 
-            'params': {'cell_id': form.attr('id'), 'input': input},
-        };
-        
-        ajax_json({
+        $.ajax({
             'url': EVAL_SERVER, 
-            'data': JSON.stringify(json_data), 
+            'data': JSON.stringify({
+                'method': 'eval_python', 
+                'params': {'cell_id': form.attr('id'), 'input': input},
+                'version': JSON_VERSION, 
+            }),
             'success': function(msg){
                 var cell_id = msg.result.cell_id;
                 form.attr('id', cell_id);
@@ -120,11 +102,6 @@ $(document).ready(function(){
                 save_worksheet();
             },
         });
-        
         return false;
     });
-    
-    
-    /** Start of code that is automatically executed */
-//    new_cell(id, input, output);
 });
