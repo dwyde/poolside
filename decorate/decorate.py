c41e1a491f483faccbe4e73c9a515a20be37e8e4
David <david@computer.(none)>
2010-11-02 20:10:48 -0400

    The "decorator" module now supports an "ALL" option.

--- decorate/decorate.py
+++ decorate/decorate.py
@@ -1,21 +1,33 @@
+ALL = '*'
+
 class allows:
     def __init__(self, *args):
         self.permitted = args
     
     def __call__(self, func):
         def modified(obj):
-            maybe = map(lambda x: isinstance(obj, x), self.permitted)
-            if filter(None, maybe): # If the above list has any "True" elements
+            if self.permitted == (ALL,):
                 func(obj)
             else:
-                print "%s not allowed for this function.  You must use one of %s." % \
-                        (type(obj), self.permitted)
+                maybe = map(lambda x: isinstance(obj, x), self.permitted)
+                if any(maybe): # If the above list has any "True" elements
+                    func(obj)
+                else:
+                    print "%s not allowed for this function.  You must use one of %s." % \
+                            (type(obj), self.permitted)
         return modified
 
-@allows(int, list)
+@allows(int, basestring)
 def print2(word):
     print word
     
+@allows(ALL)
+def print_all(word):
+    print word
+
 if __name__ == '__main__':
     print2('hello')
     print2(['hello'])
+    print '*' * 5
+    print_all('hello')
+    print_all(['hello'])
