d25e8d432c08d5415234b905a8ec37f8c70b0d30
David <david@computer.(none)>
2010-11-02 20:23:09 -0400

    Trying to make test.py use the decorators...

--- decorate/test.py
+++ decorate/test.py
@@ -2,6 +2,8 @@ import matplotlib
 matplotlib.use('Agg')
 import StringIO
 
+from decorate import allows, ALL
+
 def image_data(self, obj):
     imgdata = StringIO.StringIO()
     obj.savefig(imgdata, format='png')
@@ -13,15 +15,11 @@ def image_data(self, obj):
 #print dir(matplotlib.pyplot)
 
 class Visualize:
-    def __call__(self, obj):
-        obj_type = obj.__class__.__name__
-        func = getattr(self, obj_type)
-        return func(obj)
-        
-    def list(self, lst):
+    @allows(basestring)
+    def html_table(self, lst):
         html = ['<td>%s</td>' % (item,) for item in lst]
         return '<table>\n%s\n</table>' % ('\n'.join(html))
             
 VIZ = Visualize()
 lst = [5, 'hello', 3.3, [1, 2, 3, 4]]
-print VIZ(lst)
+print VIZ.html_table(lst)
