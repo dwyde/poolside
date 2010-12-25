1a93e9a56113a39c182ff99fb364927bdb64a212
David <david@computer.(none)>
2010-12-24 19:01:31 -0800

    Looks like I needed to pass the io_loop kwarg to the tornado HTTPServer.

--- service/ws_tornado.py
+++ service/ws_tornado.py
@@ -11,12 +11,20 @@ KERNEL_IP = '127.0.0.1'
 XREQ_PORT = 5575
 SUB_PORT = 5576
 
+# Initialize ZMQ sockets
+ctx = zmq.Context()
+request_socket = ctx.socket(zmq.XREQ)
+request_socket.connect('tcp://%s:%d' % (KERNEL_IP, XREQ_PORT))
+sub_socket = ctx.socket(zmq.SUB)
+sub_socket.connect('tcp://%s:%d' % (KERNEL_IP, SUB_PORT))
+sub_socket.setsockopt(zmq.SUBSCRIBE, '')
 
 class EchoWebSocket(websocket.WebSocketHandler):
     def open(self):
         print "WebSocket opened"
 
     def on_message(self, message):
+        print message
         msg = json.loads(message)
         request_socket.send_json(msg)
         #self.write_message(u"You said: " + message)
@@ -47,21 +55,13 @@ class ZMQLoop(tornado.ioloop.IOLoop):
         tornado.ioloop.IOLoop.__init__(self, impl=zmq.Poller())
 
 def main():
-    # Initialize ZMQ sockets
-    ctx = zmq.Context()
-    request_socket = ctx.socket(zmq.XREQ)
-    request_socket.connect('tcp://%s:%d' % (KERNEL_IP, XREQ_PORT))
-    sub_socket = ctx.socket(zmq.SUB)
-    sub_socket.connect('tcp://%s:%d' % (KERNEL_IP, SUB_PORT))
-    sub_socket.setsockopt(zmq.SUBSCRIBE, '')
-    
     # Tornado
     loop = ZMQLoop.instance()
     stream = zmqstream.ZMQStream(sub_socket, loop)
     handlers = [(r'/test', EchoWebSocket),]
     application = ZMQApplication(stream, handlers)
-       
-    http_server = tornado.httpserver.HTTPServer(application)
+    
+    http_server = tornado.httpserver.HTTPServer(application, io_loop=loop)
     http_server.listen(9996)
     loop.start()
     
