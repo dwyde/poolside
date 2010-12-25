b6573bc359b46feb0075c4c1a9bed65f2e8b2f2c
David <david@computer.(none)>
2010-12-24 18:50:03 -0800

    Doesn't actually work, but progress on tornado...?

--- service/ws_tornado.py
+++ service/ws_tornado.py
@@ -5,18 +5,12 @@ from tornado import websocket
 import json
 import zmq
 from zmq.eventloop import zmqstream, ioloop, stack_context
+
 # ZMQ socket address constants
 KERNEL_IP = '127.0.0.1'
 XREQ_PORT = 5575
 SUB_PORT = 5576
 
-ctx = zmq.Context()
-
-request_socket = ctx.socket(zmq.XREQ)
-request_socket.connect('tcp://%s:%d' % (KERNEL_IP, XREQ_PORT))
-sub_socket = ctx.socket(zmq.SUB)
-sub_socket.connect('tcp://%s:%d' % (KERNEL_IP, SUB_PORT))
-sub_socket.setsockopt(zmq.SUBSCRIBE, '')
 
 class EchoWebSocket(websocket.WebSocketHandler):
     def open(self):
@@ -32,7 +26,6 @@ class EchoWebSocket(websocket.WebSocketHandler):
 
 
 class ZMQApplication(tornado.web.Application):
-    
     def __init__(self, zmq_stream, handlers, *args, **kwargs):
         tornado.web.Application.__init__(self, handlers, *args, **kwargs)
         self._zmq_stream = zmq_stream
@@ -54,19 +47,25 @@ class ZMQLoop(tornado.ioloop.IOLoop):
         tornado.ioloop.IOLoop.__init__(self, impl=zmq.Poller())
 
 def main():
+    # Initialize ZMQ sockets
+    ctx = zmq.Context()
+    request_socket = ctx.socket(zmq.XREQ)
+    request_socket.connect('tcp://%s:%d' % (KERNEL_IP, XREQ_PORT))
+    sub_socket = ctx.socket(zmq.SUB)
+    sub_socket.connect('tcp://%s:%d' % (KERNEL_IP, SUB_PORT))
+    sub_socket.setsockopt(zmq.SUBSCRIBE, '')
+    
+    # Tornado
     loop = ZMQLoop.instance()
-    
     stream = zmqstream.ZMQStream(sub_socket, loop)
-    
     handlers = [(r'/test', EchoWebSocket),]
     application = ZMQApplication(stream, handlers)
        
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(9996)
-    
-    #loop = ioloop.IOLoop.instance()
-    #sl = SetupLoop(loop)
     loop.start()
+    
+    #tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
     main()
