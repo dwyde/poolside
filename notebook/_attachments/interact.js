9d8aad7996473cdab264cc04a079baf33eb8dfbe
David <david@computer.(none)>
2010-12-26 12:36:41 -0800

    Consolidated the WebSocket Javascript with the JQuery UI code.

--- notebook/_attachments/interact.js
+++ notebook/_attachments/interact.js
@@ -15,9 +15,37 @@ function output_cell(response) {
   }
 }
 
+function WebSocketClient(address) { 
+  this.connection = new WebSocket('ws://' + address);
+  this.connection.onopen = function() {};
+  this.connection.onmessage = function(event) {
+      var result = JSON.parse(event.data);
+      if (result) {
+          output_cell(result);
+      }
+  };
+  this.connection.onclose = function() {
+      output_cell({target: '__messages', output: 'WebSocket closed'});
+  };
+  this.connection.onerror = function(event) {
+      output_cell(event);
+  };
+}
+
+WebSocketClient.prototype.send = function(request_type, input, cell_id) {
+  var data = {
+    lang: 'python',
+    code: input,
+    caller: cell_id,
+  };
+  this.connection.send(JSON.stringify(data));
+};
+
+
+/* Main JQuery code */
 $(document).ready(function(){
-  WS_CLIENT.connect(WS_ADDRESS, output_cell);
-  var db = $.couch.db(DATABASE);
+  var ws_client = new WebSocketClient(WS_ADDRESS);
+  //var db = $.couch.db(DATABASE);
   
   function save_worksheet() {
     var cells = $('#worksheet')
@@ -27,10 +55,10 @@ $(document).ready(function(){
               }).get();
     
     var data = {cells: cells, type: 'worksheet'};
-    db.openDoc(WORKSHEET_NAME, { success: function(doc) {
+    /*db.openDoc(WORKSHEET_NAME, { success: function(doc) {
       $.extend(data, {_id: doc._id, _rev: doc._rev});
       db.saveDoc(data);
-    }});
+    }});*/
   }
   
   /* Handler for submission of the input form. */
@@ -38,7 +66,7 @@ $(document).ready(function(){
     var choice = 'execute_request';
     var input = $(this).children('.input').val();
     var id = $(this).attr('id');
-    WS_CLIENT.ipython.send(choice, input, id);
+    ws_client.send(choice, input, id);
     $(this).children('p.output').html('');
 
     /* Prevent actual submission of the form. */
@@ -46,7 +74,7 @@ $(document).ready(function(){
   });
   
   $('#add_cell').click(function(){
-    db.saveDoc({type: 'cell', input: '', output: ''});
+    //db.saveDoc({type: 'cell', input: '', output: ''});
     var cell = new_cell('', '', '');
     $('#worksheet').append(cell);
   });
