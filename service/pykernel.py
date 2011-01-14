ae22ec3b301528d19e85005c2fb6b1fff7f243c7
David Wyde <david.wyde@gmail.com>
2011-01-14 16:58:53 -0500

    Add to pykernel's docstring.

--- service/pykernel.py
+++ service/pykernel.py
@@ -19,6 +19,19 @@ _SETUP_IP = ('127.0.0.1', 0)
 
 def interpreter(pipe):
     """Set up an :class:`IPython.kernel.core.interpreter.Interpreter`.
+    
+    The kernel will listen for connections on a random local port.
+    This process will communicate with the main WebSocket server via
+    the `pipe` variable, a :class:`multiprocessing.Connection`.
+    
+    The kernel's global namespace contains an instance of the 
+    class :class:`visualize.dec.Viz`.  This variable `Viz` is callable::
+        
+        >>> a = range(5)
+        >>> print Viz(a)
+        ...
+    
+    Otherwise, this is a normal IPython interpreter.
     """
     
     listener = Listener(_SETUP_IP)
@@ -40,4 +53,4 @@ def interpreter(pipe):
             'target': caller,
             'type': 'output',
         })
-        
+
