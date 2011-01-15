88729b5a549fde9c004a1d3fcdcc3a10a21e780e
David Wyde <david.wyde@gmail.com>
2011-01-15 03:15:00 -0500

    Cosmetic changes to Viz code.

--- visualize/dec.py
+++ visualize/dec.py
@@ -8,33 +8,33 @@ import os
 import glob
 import collections
 
-
 class Viz:
+    """ Construct a dynamic viz dispatching function """
+    
     def __init__(self, extdirs):
-        """ Construct a dynamic viz dispatching function """
         self.typemap = collections.defaultdict(list)
         extFiles = []
+        # Make this class available in extension functions.
         globalsDict = {'Viz': self}
         localsDict = {}
         for d in extdirs:
-          extFiles.extend(glob.glob(os.path.join(d, '*.py')))
+            extFiles.extend(glob.glob(os.path.join(d, '*.py')))
         for f in extFiles:
-          execfile(f, globalsDict, localsDict)
+            execfile(f, globalsDict, localsDict)
         for name, func in localsDict.items():
             if type(func) == types.FunctionType:
-                for t,acceptor in func(None).items():
+                for t, acceptor in func(None).items():
                     self.typemap[t].append((acceptor, func))
                 setattr(self, name, func)
+    
     def __call__(self, obj):
         if type(obj) in self.typemap:
             for acceptor, func in self.typemap[type(obj)]:
                 if acceptor(obj):
-                    return(func(obj))
-        ## Do The default thing
-        return(str(obj))
+                    return func(obj)
 
-
-
+        # Do the default thing.
+        return str(obj)
 
 class VizDecor:
     """ A class that creates decorator functions for visualizable objects """
@@ -57,4 +57,4 @@ class VizDecor:
                 else:
                     raise TypeError('object type not supported')
 
-        return(newFunc)
+        return newFunc
