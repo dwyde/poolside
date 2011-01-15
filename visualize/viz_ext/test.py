01249d7e69b2e59cffe5c312768e9db9758f8543
Ollie <ollie@ubuntu.ubuntu-domain>
2011-01-15 16:08:55 -0500

    Make viz render lists as tables.

--- visualize/viz_ext/test.py
+++ visualize/viz_ext/test.py
@@ -1,24 +1,28 @@
 import os
 import sys
+import json
 
 from dec import VizDecor
 
 @VizDecor({list: lambda x:True})
 def table(obj):
-    s = '<table>\n'
-    for row in obj:
-        s += ' <tr>\n'
-        if isinstance(row, list):
-            for col in row:
-                s += '  <td>%s</td>\n' % (col,)
+    s = '<div><table class="viz_list"><tr><br>'
+    for item in obj:
+        s += '<td>'
+        if isinstance(item,list):
+            # two dimensional
+            s += Viz(item)
         else:
-            s += '  <td>%s</td>\n' % (row,)
-        s += ' </tr>\n'
-    s += '</table>'
-    return s
+            # still one dimensional
+            s += '%s'%(str(item))
+        s += '</td>'
+    s+='</tr></table></div></br>'
+    return(s)
+
+#@VizDecor({dict: lambda x:True})
 
 if __name__ == '__main__':
     a = range(5)
-    b = ['a', [1, 2], 'b']
-    print table(a), '\n'
+    b = ['a',[1,2],'b']
+    print table(a)
     print table(b)
