91d8a42191874a652d78617f193a9ea149b39a85
David Wyde <david.wyde@gmail.com>
2011-01-14 23:50:10 -0500

    Move Viz extensions directory name into its own variable.

--- visualize/viz_extension.py
+++ visualize/viz_extension.py
@@ -7,9 +7,12 @@ import os
 
 import dec
 
-def load_ipython_extension(user_ns):
-    '''A setup function, called each time this extension is loaded.'''
+_EXTENSION_DIR = 'viz_ext'
+
+def load_ipython_extension(namespace):
+    """A setup function, called each time this extension is loaded."""
+    
     basedir = os.path.split(os.path.abspath(__file__))[0]
-    path = os.path.join(basedir, 'viz_ext')
+    path = os.path.join(basedir, _EXTENSION_DIR)
     viz = dec.Viz([path])
-    user_ns['Viz'] = viz
+    namespace['Viz'] = viz
