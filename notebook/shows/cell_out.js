0828662aebf0e886721b9d07a47630e6908fa378
David Wyde <david.wyde@gmail.com>
2011-01-26 03:04:16 -0500

    Try to protect the notebook from malicious JavaScript.

--- notebook/shows/cell_out.js
+++ notebook/shows/cell_out.js
@@ -1,11 +1,17 @@
 function(doc, req) {
+    // !code _attachments/mustache.js
     // !json templates.display.iframe_cell
     
+    var cellOut = doc.output.replace(/"/g, '\\"').replace(/\n/g, '')
+                .replace(/</g, '&lt;') || '';
+
+    var output = {
+        'output': cellOut,
+    };
     
-    
-    send(templates.display.iframe_cell);
-    
-    var output = doc.output || '';
-    
-    send(output + '</body></html>');
+    return {
+        "headers" : {"Content-Type" : "text/html"},
+        "body" : Mustache.to_html(templates.display.iframe_cell, output),
+    }
 }
+ 
