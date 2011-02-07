1e663b18a146b9955c2b50bd1d3fa62cb123a062
David Wyde <david.wyde@gmail.com>
2011-02-07 17:13:47 -0500

    There is no input type="textarea"...

--- notebook/_attachments/new_cell.js
+++ notebook/_attachments/new_cell.js
@@ -2,7 +2,7 @@
         var cell = '<div class="cell" id="' + id + '"><form method="POST"> \
          <button type="submit" class="evaluate">Evaluate</button> \
          <button class="delete">Delete</button> \
-         <input type="textarea" class="input" value="' + 
+         <input type="text" class="input" value="' + 
          input.replace(/"/g, '&quot;') + '" /></form> \
          <div class="output">' + output + '</div></div>';
          
