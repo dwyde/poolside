0a7fbca6b442dd5105aea23e32dcebcaa36c7931
David Wyde <david.wyde@gmail.com>
2011-02-07 22:14:17 -0500

    Set CouchDB port and server port via command line args.

--- standalone/http_server.py
+++ standalone/http_server.py
@@ -4,6 +4,8 @@ import threading
 
 import cgi
 import json
+import optparse
+import sys
 
 from manager import KernelController
 
@@ -13,7 +15,8 @@ class Handler(BaseHTTPRequestHandler):
     
     def do_POST(self):
         self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', 'http://localhost:5984')
+        self.send_header('Access-Control-Allow-Origin', 'http://localhost:%d' %
+                self.server.couch_port)
         #self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         
@@ -35,15 +38,38 @@ required for Python requests.')
 
     def do_OPTIONS(self):
         self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', 'http://localhost:5984')
+        self.send_header('Access-Control-Allow-Origin', 'http://localhost:%d' %
+                self.server.couch_port)
         #self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
 
+    def set_couch_port(self, couch_port):
+        self.couch_port = couch_port
+
+def parse_arguments():
+    """Process command line arguments using :class:`optparse.OptionParser`.
+    """
+    
+    parser = optparse.OptionParser()
+    parser.add_option('-c', '--couch_port', dest='couch_port', default='5984', 
+            metavar='COUCH_PORT', help='CouchDB server port (on localhost)', 
+            type='int')
+    parser.add_option('-s', '--service_port', dest='service_port', metavar='SERVICE_PORT',
+            help='port on which this HTTP server runs', default=8080, type='int')
+
+    (options, args) = parser.parse_args()
+    
+    del sys.argv[1:]
+    
+    return options
+
 def main():
-    server = ThreadedHTTPServer(('localhost', 8080), Handler)
+    options = parse_arguments()
+    server = ThreadedHTTPServer(('localhost', options.service_port), Handler)
+    server.set_couch_port(options.couch_port)
     print 'Starting server, use <Ctrl-C> to stop'
     server.serve_forever()
 
