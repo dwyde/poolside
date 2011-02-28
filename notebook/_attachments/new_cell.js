8484b9d8ccd197be04b3381bec8471dbf9417c17
David Wyde <david.wyde@gmail.com>
2011-02-27 22:48:40 -0500

    Remove secondary class from cell "submit" buttons.

--- notebook/_attachments/new_cell.js
+++ notebook/_attachments/new_cell.js
@@ -11,9 +11,9 @@
       
         return '<div class="cell" id="'+ html_escape(id) + '"> \
               <form method="POST"> \
-                <button type="submit" class="eval text">txt</button> \
-                <button type="submit" class="eval python">py</button> \
-                <button type="submit" class="eval ruby">rb</button> \
+                <button type="submit" class="text">txt</button> \
+                <button type="submit" class="python">py</button> \
+                <button type="submit" class="ruby">rb</button> \
                 <button class="delete">X</button> \
                 <textarea class="input">' + html_escape(input) + '</textarea> \
               </form> \
