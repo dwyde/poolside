1be9ee2cafdcbe4f0bb557d24ad4c627d29ddf7e
David Wyde <david.wyde@gmail.com>
2011-03-21 02:12:36 -0400

    Fix a collation bug with worksheets: cell index was interpreted as a string.
    E.g., "5" is a now number, so it follows "46" -- not the case with strings.

--- notebook/views/get_cells/map.js
+++ notebook/views/get_cells/map.js
@@ -1,7 +1,7 @@
 function(doc) {
     if (doc.type == 'worksheet') {
         for (var i in doc.cells) {
-            emit([doc._id, i], {_id: doc.cells[i]});
+            emit([doc._id, parseInt(i)], {_id: doc.cells[i]});
         }
     }
 }
