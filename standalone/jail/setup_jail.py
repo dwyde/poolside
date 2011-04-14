b9559148be5aee63535c84ea168632f2f7478466
David Wyde <david.wyde@gmail.com>
2011-04-14 14:32:40 -0400

    Modify preliminary test of setting up a chroot jail.

--- standalone/jail/setup_jail.py
+++ standalone/jail/setup_jail.py
@@ -1,45 +1,28 @@
 import os
 import pwd
+import grp
 
 NEW_USER = 'nobody'
-NEW_ROOT = 'new_root'
+NEW_GROUP = 'nobody'
+NEW_ROOT = 'copies'
 
 def jail_dir(new_root):
     current_dir = os.path.abspath(os.getcwd())
     return os.path.join(current_dir, new_root)
 
 def setup_jail():
+    # Choose a new user, group, and root directory.
     new_uid = pwd.getpwnam(NEW_USER)[2]
+    new_gid = grp.getgrnam(NEW_GROUP)[2]
     root_dir = jail_dir(NEW_ROOT)
-    
-    print 'uid:  %s\nroot: %s' % (new_uid, root_dir)
 
+    # Set up the jail.
     os.chdir(root_dir)
     os.chroot(root_dir)
+    os.setgid(new_gid)
     os.setuid(new_uid)
-
-    #os.environ['PATH'] = '/usr/bin'
-
-    del new_uid
-    del root_dir
-
-def try_stuff():
-    #os.chdir('/etc')
-    #Popen(['cd', '..'])
-    #Popen(['python', ]
-    #import sys
-    #print sys.path
-    
-    import os
-    print os.system('ls')
-    print os.environ
-    
-    import sys
-    print sys.path
-    #import math
-    #print math.sqrt(10)
 
 if __name__ == '__main__':
     setup_jail()
-
-    try_stuff()
+    print 'Working directory: %s\nUser id: %d\nGroup id: %d' % \
+              (os.getcwd(), os.getuid(), os.getgid())
