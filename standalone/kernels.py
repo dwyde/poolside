8cd42ee9b404ffee2af5fdb4afef464e6fb3881a
David Wyde <david.wyde@gmail.com>
2011-05-16 00:06:46 -0400

    Add simple docstrings to the Kernel class.

--- standalone/kernels.py
+++ standalone/kernels.py
@@ -16,29 +16,44 @@ class Kernel(Popen):
         Popen.__init__(self, arg_list, stdout=PIPE, stdin=PIPE,
                 preexec_fn=preexec_fn)
     
-    def eval_code(self, code):
-        self.send(code)
-        result = self.recv()
-        return self._eval_as_python(result)
-    
     def send(self, message):
+        """Encode and send a message to a kernel process."""
+        
         command = self._encode(message)
         self.stdin.write('%s\n' % command)
     
     def recv(self):
+        """Receive and decode a message from a kernel process.
+        
+        Block until a newline-terminated message is available.
+        """
+        
         message = self.stdout.readline()[:-1]
         return self._decode(message)
 
     def _encode(self, message):
+        """Prepare a string of code for the kernel process."""
+        
         command = message.replace(self._TO_REPLACE, self._DUMMY_CHAR)
         return command.encode(self._ENCODING)
     
     def _decode(self, message):
+        """Prepare a kernel's message string for external use."""
+        
         command = message.decode(self._ENCODING)
         return command.replace(self._DUMMY_CHAR, self._TO_REPLACE)
 
-    def _eval_as_python(self, s):
+    def _eval_as_python(self, data_string):
+        """Safely try to evaluate strings as builtin data types."""
+        
         try:
-            return ast.literal_eval(s)
+            return ast.literal_eval(data_string)
         except (SyntaxError, ValueError):
-            return s
+            return data_string
+
+    def eval_code(self, code):
+        """Public method to evaluate code with this kernel."""
+        
+        self.send(code)
+        result = self.recv()
+        return self._eval_as_python(result)
