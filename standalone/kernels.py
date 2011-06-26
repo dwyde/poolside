e6eb3037f8b06ee4865af8ae9c5c9e09ee8ea385
David Wyde <david.wyde@gmail.com>
2011-06-26 14:58:18 -0700

    Remove a default value for a kwarg (i.e., make it into an arg).

--- standalone/kernels.py
+++ standalone/kernels.py
@@ -51,7 +51,7 @@ def respond(content, msg_type='output'):
 class KernelController(dict):
     """A class to wrap `Kernel`s for external use."""
     
-    def __init__(self, path_prefix='jail/kernels'):
+    def __init__(self, path_prefix):
         """Class constructor: initialize kernels."""
         
         dict.__init__(self)
